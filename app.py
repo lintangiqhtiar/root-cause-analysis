1bfdb8c00646fc917f711c8ef74fab65c36379bc
Lintang Iqhtiar Dwi Mawarni <104833840+lintangiqhtiar@users.noreply.github.com>
2025-06-12 06:55:50 +0700

    Update app.py
    make it simple

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
-"""
-H1 : input file yang sudah melalui preprocessing (yakni hingga data telah ber sentimen negatif)
-H2 : input berupa file json
-
-"""
-
 import streamlit as st
 import pandas as pd
 import os
+from utils.recommendation import generate_recommendation
 
-st.set_page_config(page_title="Upload data", layout="wide")
-st.title("❄ Upload data komentar google playstore")
 
-#Upload file
-uploaded_file = st.file_uploader("Upload CSV file", type=['csv'])
+# st.set_page_config(page_title="Upload data", layout="wide")
+st.title("❄ Prototype Aplikasi Rekomendasi akar permasalahan pengguna Aplikasi Digital Korlantas POLRI")
+st.subheader("Hasil analisis pemodelan topik LDA dan Guided LDA")
 
-if uploaded_file is not None:
-    file_path = os.path.join("data", "uploaded_data-test.csv")
-    df = pd.read_csv(uploaded_file)
-    df.to_csv(file_path, index= False)
+# import file excel
+df = pd.read_excel("data/rekap_lda_glda.xlsx")
+# Memberi fitur untuk seleksi isi kolom LDA atau GLDA
+lda_option = st.selectbox("Pilih model LDA atau GLDA", ["LDA", "Guided LDA", "LDA & GLDA"])
+if lda_option == "LDA":
+    st.subheader("Hasil analisis LDA")
+    df = df[df["model"] == "LDA"]
+elif lda_option == "GLDA":
+    st.subheader("Hasil analisis GLDA")
+    df = df[df["model"] == "GLDA"]
+elif lda_option == "LDA & GLDA":
+    st.subheader("Hasil analisis LDA & GLDA")
+    df = df[df["model"].isin(["LDA", "GLDA"])]
 
-    st.success("File berhasil diupload silahkan lanjut ke halaman analisis")
-    st.page_link("pages/analisis.py", label="Lihat hasil analisis")
+st.dataframe(df)
+
+# Generate Recommendation
+st.subheader("🔧 Sistem Rekomendasi Perbaikan")
+st.markdown("Masukkan keluhan pengguna, dan sistem akan memberikan rekomendasi perbaikannya.")
+
+# Input dari pengguna
+keluhan_input = st.text_area("Keluhan Pengguna", placeholder="Contoh: Aplikasi crash saat dibuka")
+
+if st.button("🔍 Generate Rekomendasi") and keluhan_input:
+    with st.spinner("Menghasilkan rekomendasi..."):
+        hasil = generate_recommendation(keluhan_input)
+        st.success("✅ Rekomendasi:")
+        st.write(hasil)
